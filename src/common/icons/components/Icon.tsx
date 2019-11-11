ee978c444189dac0cd89c55c247dc8cec5b8a650
Enitoni <enitoni@gmail.com>
2019-11-11 13:40:05 +0100

    add icon system

---
+++ src/common/icons/components/Icon.tsx
@@ -0,0 +1,20 @@
+import { IconType } from "../types/IconType"
+import * as icons from "../icons"
+import React from "react"
+
+export interface IconProps {
+  className?: string
+  name: IconType
+}
+
+export function Icon(props: IconProps) {
+  const { name, className } = props
+
+  if (!icons[name]) return null
+
+  return React.cloneElement(icons[name], {
+    className,
+    width: "100%",
+    height: "100%",
+  })
+}
