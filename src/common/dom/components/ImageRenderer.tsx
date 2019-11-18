1089d8eafb2b9b8b6ede26522848281073a9f72c
Enitoni <enitoni@gmail.com>
2019-11-19 00:38:44 +0100

    make ImageRenderer more smooth

--- src/common/dom/components/ImageRenderer.tsx
+++ src/common/dom/components/ImageRenderer.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react"
 import styled from "../../../modules/theming/custom"
 import React from "react"
+import { cover } from "polished"
+import { TransitionGroup, Transition } from "react-transition-group"
+import { TransitionStatus } from "react-transition-group/Transition"
 
 export type ImageRendererProps = {
   src: string
@@ -8,44 +11,73 @@ export type ImageRendererProps = {
   alt: string
 }
 
-const Container = styled.div<{ loaded: boolean }>`
-  transition: 200ms ease opacity;
-  opacity: ${props => (props.loaded ? 1 : 0)};
+const Container = styled.div`
+  position: relative;
+  overflow: hidden;
+`
 
+const RenderedImage = styled.div<{ status: TransitionStatus }>`
+  ${cover()}
+
+  transition: 500ms ease opacity;
   background-size: cover;
+
+  ${props => {
+    const { status } = props
+
+    if (status === "entering")
+      return `
+      opacity: 1;
+    `
+
+    if (status === "entered")
+      return `
+      opacity: 1;
+    `
+
+    if (status === "exiting")
+      return `
+      opacity: 0;
+    `
+  }}
 `
 
 export function ImageRenderer(props: ImageRendererProps) {
   const { src, alt, className } = props
 
   const [image] = useState(() => document.createElement("img"))
-  const [loaded, setLoaded] = useState(image.complete)
   const [currentSrc, setCurrentSrc] = useState("")
 
   useEffect(() => {
     image.onload = () => {
-      setLoaded(true)
       setCurrentSrc(src)
     }
 
-    setLoaded(false)
-
-    const timer = window.setTimeout(() => {
-      image.src = src
-    }, 200)
-
-    return () => window.clearTimeout(timer)
+    image.src = src
   }, [image, src])
 
+  const renderCurrent = () => {
+    if (!currentSrc) return
+
+    return (
+      <Transition key={currentSrc} timeout={500}>
+        {status => (
+          <RenderedImage
+            status={status}
+            role="img"
+            aria-label={alt}
+            style={{
+              backgroundImage: `url(${currentSrc})`,
+            }}
+          />
+        )}
+      </Transition>
+    )
+  }
+
   return (
-    <Container
-      role="img"
-      loaded={loaded}
-      aria-label={alt}
-      className={className}
-      style={{
-        backgroundImage: `url(${currentSrc})`,
-      }}
-    />
+    <Container className={className}>
+      <TransitionGroup>{renderCurrent()}</TransitionGroup>
+    </Container>
   )
 }
