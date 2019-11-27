90c7db13f9cdb884a8f0f686e81864ec53211765
Enitoni <enitoni@gmail.com>
2019-11-27 17:54:19 +0100

    add WaveDistrict screenshots

--- src/modules/portfolio/entries/waveDistrict.tsx
+++ src/modules/portfolio/entries/waveDistrict.tsx
@@ -5,6 +5,7 @@ import { typescript, node, react, webpack, postgres, mobx } from "../technologie
 export const waveDistrict: Project = {
   key: "wavedistrict",
   period: ["11.2015", "09.2019"],
+  screenshots: ["Explore page", "User page", "Track page"],
   name: "WaveDistrict",
   links: [
     {
